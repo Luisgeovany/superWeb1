b6c77717eb6c0339ec35f99df5c15e68cf59fe86
luisGeovany <glopezvan@gmail.com>
2020-09-15 15:01:41 -0500

    Se creo un ejemplo de clase

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+
+$producto1="Arroz";
+$producto2="Frijoles";
+$producto3="Huevos";
+$producto4="Panela";
+$producto5="Cafe";
+$producto6="Pasta";
+$producto7="Aguardiente";
+$producto8="Papas";
+$producto9="Carne";
+$producto10="Sal";
+
+$precio1=6000;
+$precio2=10000;
+$precio3=8000;
+$precio4=4000;
+$precio5=12500;
+$precio6=6000;
+$precio7=15000;
+$precio8=6000;
+$precio9=25000;
+$precio10=800;
+
+$total= $producto1 + $producto2 + $producto3 + $producto4 + $producto5 + $producto6 + $producto7 + $producto8 + $producto9 + $producto10;
+
+$total=50000;
+if($total<=80000){
+    echo("Gracias por su compra");
+}else{
+    echo("Debe pedir fiado");
+}
+
+
+?>
